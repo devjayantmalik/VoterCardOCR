54bb1848667695cc21a5d6b1b0642698ac544a18
Jayant Malik <dev.jayantmalik@gmail.com>
2024-07-19 10:29:37 +0530

    added sample sql files to insert devices

---
+++ sql/devices-insert-query.sql
@@ -0,0 +1,7 @@
+-- 600 batch_size is for 12GB GPU and 1100 batch_size for 24GB(19GB used)
+INSERT INTO devices(id, name, batch_size) VALUES (1, 'cdis-shobu', 600);
+INSERT INTO devices(id, name, batch_size) VALUES (2, 'cdis-bharat', 600);
+INSERT INTO devices(id, name, batch_size) VALUES (3, 'cdis-tanmay', 50);
+INSERT INTO devices(id, name, batch_size) VALUES (4, 'cdis-ravi', 50);
+INSERT INTO devices(id, name, batch_size) VALUES (5, 'cdis-server', 1100);
+
